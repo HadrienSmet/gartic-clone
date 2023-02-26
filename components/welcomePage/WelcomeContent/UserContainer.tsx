7db7140a7861dac2091491a89abd8ab7b765f221
Hadrien <smethadrien.96@gmail.com>
2023-02-26 15:26:02 +0100

    set the basic JS logic to switch the avatar picture and also to switch the class active on the 'header' of the user-container

--- components/welcomePage/WelcomeContent/UserContainer.tsx
+++ components/welcomePage/WelcomeContent/UserContainer.tsx
@@ -1,25 +1,85 @@
 import Image from "next/image";
-import React from "react";
+import React, {
+    MouseEvent,
+    MutableRefObject,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import { FaRedoAlt } from "react-icons/fa";
 
+const useUserContainer = () => {
+    const anonymRef = useRef<HTMLSpanElement | null>(null);
+    const authRef = useRef<HTMLSpanElement | null>(null);
+    const [avatarIndex, setAvatardIndex] = useState<number>(0);
+
+    const handleNewAvatar = () => {
+        const newIndex = Math.floor(Math.random() * 15);
+        setAvatardIndex(newIndex);
+    };
+
+    const handleUserContainerNav = (e: MouseEvent) => {
+        anonymRef.current?.classList.remove("active");
+        authRef.current?.classList.remove("active");
+        const target = e.target as HTMLSpanElement;
+        if (target.id === "anonym") {
+            anonymRef.current?.classList.add("active");
+        } else {
+            authRef.current?.classList.add("active");
+        }
+    };
+
+    useEffect(() => {
+        anonymRef.current?.classList.add("active");
+    }, []);
+
+    return {
+        anonymRef,
+        authRef,
+        avatarIndex,
+        handleNewAvatar,
+        handleUserContainerNav,
+    };
+};
+
 const UserContainer = () => {
+    const {
+        anonymRef,
+        authRef,
+        avatarIndex,
+        handleNewAvatar,
+        handleUserContainerNav,
+    } = useUserContainer();
     return (
         <div className="user">
             <div className="user__header">
-                <span className="active" id="anonym">
+                <span
+                    onClick={(e) => handleUserContainerNav(e)}
+                    ref={anonymRef}
+                    id="anonym"
+                >
                     anonyme
                 </span>
-                <span id="auth">authentification</span>
+                <span
+                    onClick={(e) => handleUserContainerNav(e)}
+                    ref={authRef}
+                    id="auth"
+                >
+                    authentification
+                </span>
             </div>
             <div className="user__content">
                 <div className="user__content__avatar-container">
                     <Image
-                        src="/images/gartic-avatar-0.svg"
+                        src={`/images/gartic-avatar-${avatarIndex}.svg`}
                         alt="character icon"
                         width={180}
                         height={180}
                     />
-                    <div className="user__content__avatar-button">
+                    <div
+                        className="user__content__avatar-button"
+                        onClick={handleNewAvatar}
+                    >
                         <FaRedoAlt />
                     </div>
                 </div>
